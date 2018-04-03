63025cb47857d1d6399069c32a075ada62ff85a0
Cordula <cordula.gloge@gmx.de>
2018-04-03 17:15:12 +0200

    Set onchangelistener for seekbar

--- app/src/main/java/de/cordulagloge/android/musicplayer/PlayActivity.java
+++ app/src/main/java/de/cordulagloge/android/musicplayer/PlayActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.SeekBar;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,6 +17,8 @@ public class PlayActivity extends AppCompatActivity {
 
     MediaPlayer songPlayer;
     ActivityPlayBinding playBinding;
+    List<Song> songArrayList;
+    int songNumber, numberOfSongs;
     Handler myHandler = new Handler();
 
     final Runnable updateSong = new Runnable() {
@@ -32,24 +35,30 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         playBinding = DataBindingUtil.setContentView(this, R.layout.activity_play);
 
+        if (getActionBar() != null) {
+            getActionBar().hide();
+        }
+
+        songNumber = 0;
+
         Bundle b = getIntent().getExtras();
-        MyParcelable songObject = b.getParcelable("parcel");
-        List<Song> songArrayList = songObject.getArrList();
-        int currentSongNumber = songObject.getMyInt();
-        Song currentSong = songArrayList.get(0);
+        MyParcelable songObject = null;
+        if (b != null) {
+            songObject = b.getParcelable("parcel");
+        }
+        songArrayList = songObject.getArrList();
+        Song currentSong = songArrayList.get(songNumber);
+        numberOfSongs = songArrayList.size();
 
         playBinding.titleTextview.setText(currentSong.getTitle());
         playBinding.albumTextview.setText(currentSong.getAlbum());
         playBinding.albumImageview.setImageBitmap(currentSong.getAlbumImage(this));
 
+
         // Initialize mediaPlayer
         songPlayer = new MediaPlayer();
-        try {
-            songPlayer.setDataSource(currentSong.getFilePath());
-            songPlayer.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        setMediaPlayer(currentSong);
+
         int maxPosition = songPlayer.getDuration();
         playBinding.seekbar.setMax(maxPosition);
 
@@ -69,11 +78,68 @@ public class PlayActivity extends AppCompatActivity {
                 }
             }
         });
+
+        songPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                    setNextSong();
+            }
+        });
+
+        // set position in song if seekbar is changed
+        playBinding.seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int seekbarPosition, boolean isInput) {
+                if (songPlayer != null && isInput){
+                    songPlayer.seekTo(seekbarPosition);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
     public void onDestroy(){
         super.onDestroy();
         songPlayer.stop();
+    }
+
+    /**
+     * set next song of playlist or stop playing and set mediaplayer to first song of playlist
+     */
+    public void setNextSong(){
+        if (songNumber < numberOfSongs) {
+            songNumber++;
+            Song currentSong = songArrayList.get(songNumber);
+            setMediaPlayer(currentSong);
+        }
+        else {
+            songNumber = 0;
+            Song currentSong = songArrayList.get(songNumber);
+            setMediaPlayer(currentSong);
+            playBinding.playButton.setImageResource(R.drawable.ic_play_arrow_black_24dp);
+        }
+    }
+
+    /**
+     * prepare mediaplayer to play current song
+     * @param currentSong Song object with data of the current song
+     */
+    public void setMediaPlayer(Song currentSong){
+        try {
+            songPlayer.setDataSource(currentSong.getFilePath());
+            songPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
